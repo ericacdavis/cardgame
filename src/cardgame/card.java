d7ff5c384ed6885c6e5dc5720834ea899d721b6d
ericadavis <ericadavis@ericas-air.fios-router.home>
2017-11-20 11:00:44 -0500

    Done!

--- src/cardgame/card.java
+++ src/cardgame/card.java
@@ -1,5 +1,101 @@
 package cardgame;
 
 public class card {
-
+	//attributes
+	private char suit;
+	private char rank;
+	
+	private static char [] SUITS = {'S', 'D', 'H', 'C'};
+	private static char [] RANKS = {'A', '2','3','4','5','6','7','8','9','T','J','Q','K'};
+	
+	//methods
+	//------- overload the constructor
+	public card () {
+		int num= (int)(Math.random() * SUITS.length);
+		suit= SUITS[num];
+		
+		num= (int)(Math.random() * RANKS.length);
+		rank= RANKS[num];
+	}
+	//constructor that assign specific values
+	public card (char s, char r) {
+		setSuit(s);
+		setRank(r);
+	}
+	//set rank within our requirements
+	public void setRank(char r) {
+		for (int i= 0; i < RANKS.length; i++) {
+			if (r== RANKS[i]) {
+				rank = r;
+				return;
+			}
+		}
+		System.out.println("Rank not found in list");
+	}
+	//get rank
+	public char getRank() {
+		return rank;
+	}
+	
+	//set suit within our requirements
+	public void setSuit(char s) {
+		for (int i= 0; i < SUITS.length; i++) {
+			if (s== SUITS[i]) {
+				suit = s;
+				return;
+			}
+		}
+		System.out.println("Suit not found in list");
+	}
+	//get suit
+	public char getSuit() {
+		return suit;
+	}
+	
+	//to String Method
+	public String toString() {
+		String cardInfo = "Rank: " + rank + "\n" + "Suit: " + suit;
+		return cardInfo;
+	}
+	
+	public boolean isGreaterThan(card c) {
+		//if index is bigger, card is bigger
+		int index1=0;
+		int index2=0;
+		for (int i = 0; i < RANKS.length; i++){
+			if(this.rank == RANKS[i]) {
+				index1= i;
+			}
+			if (c.getRank()== RANKS[i]) {
+				index2 = i;
+			}
+		}
+		//if the index is bigger in ranks
+		if(index1> index2) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public boolean isEqualTo(card c) {
+		int index1=0;
+		int index2=0;
+		for (int i = 0; i < RANKS.length; i++){
+			if(this.rank == RANKS[i]) {
+				index1= i;
+			}
+			if (c.getRank()== RANKS[i]) {
+				index2 = i;
+			}
+		}
+		//if the index is bigger in ranks
+		if(index1 == index2) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
 }
--- src/cardgame/wargame.java
+++ src/cardgame/wargame.java
@@ -4,7 +4,35 @@ public class wargame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*
+		card c1 = new card ();
+		System.out.println(c1);
+		
+		char rank = 'S';
+		char suit = 'D';
+		card c2 = new card(suit,rank);
+		System.out.println(c2);
+		*/
+		//War game
+		int player1 = 0;
+		int player2=0;
+		for (int i =0; i< 20; i++) {
+			card c1= new card();
+			card c2 = new card();
+		System.out.println("Card 1: " + c1);
+		System.out.println("Card 2: " + c2);
+		
+		if(c1.isEqualTo(c2)) {
+			System.out.println("Tie");
+		}
+		else if(c1.isGreaterThan(c2)) {
+			player1++;
+		}else {
+			player2++;
+		}
+		System.out.println("player 1 score: " + player1);
+		System.out.println("player 2 score: " + player2);
+		System.out.println(" ");
 	}
-
+	}
 }
